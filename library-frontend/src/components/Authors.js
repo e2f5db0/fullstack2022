b37c668c2526fbb33962c0169ec5fc93b75761f9
e2f5db0 <sami.toiskallio@protonmail.com>
2022-01-19 18:33:23 +0200

    Complete exercises 8.8-8.12

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,83 @@
+  
+import { useQuery, useMutation } from '@apollo/client'
+import React, { useState } from 'react'
+import { ALL_AUTHORS, UPDATE_AUTHOR } from '../queries'
+
+const Authors = (props) => {
+
+  const [name, setName] = useState('')
+  const [setBornTo, setSetBornTo] = useState('')
+  
+  const result = useQuery(ALL_AUTHORS)
+  const [ updateAuthor ] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [ { query: ALL_AUTHORS } ]
+  })
+
+  const submit = async (event) => {
+    event.preventDefault()
+    
+    updateAuthor({ variables: { name, setBornTo } })
+
+    setName('')
+    setSetBornTo('')
+  }
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  
+  const authors = result.data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <select
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          >
+            {authors.map(a => <option key={a.name}>{a.name}</option>)}
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            type='number'
+            value={setBornTo}
+            onChange={({ target }) => setSetBornTo(parseInt(target.value))}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
